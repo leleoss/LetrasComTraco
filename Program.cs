31aebabacd44ecd67e8b489cd8a2c7ba246a9ab8
user_acronus <user_acronus@senacsp.edu.br>
2018-04-23 13:40:06 -0300

    Letras com traço

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LetrasComTraco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            Console.Clear();
+
+            Console.WriteLine("Digite um texto");
+            texto = Console.ReadLine();
+            for(int i = 0 ; i < texto.Length; i++)
+                Console.Write(texto[i]+"-");
+        }
+    }
+}
